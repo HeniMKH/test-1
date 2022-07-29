a6af2323d47b42c8ec3c07825e10504770c12097
HeniMKH <108895187+HeniMKH@users.noreply.github.com>
2022-07-29 20:25:07 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,99 @@
+const {
+  core: { test, expect, run },
+  prettify
+} = window.jestLite;
+
+/* =================
+  FIND ELEMENTS
+  These are all the elements we will look for.
+   ===================
+*/
+const getHeader = document.querySelectorAll("header"),
+  getH1 = document.querySelectorAll("h1"),
+  getSiteHeader = document.querySelectorAll(".c-site-header"),
+  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
+  getMain = document.querySelectorAll("main"),
+  getFooter = document.querySelectorAll("footer"),
+  getSiteFooter = document.querySelectorAll(".c-site-footer"),
+  getIFrame = document.querySelectorAll("iframe"),
+  getImage = document.querySelectorAll("img"),
+  getWords = document.body.innerText;
+
+/* =================
+   ASSERTIONS 
+   These are the things we check are true about your page.
+   Read and update your HTML to discover the requirements.
+   The tests will run every time you update your code.
+   ===================
+*/
+test("There is at least one header element", () => {
+  expect(getHeader.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one h1", () => {
+  expect(getH1.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one header element with the class c-site-header", () => {
+  expect(getSiteHeader.length).toBe(1);
+});
+test("There is a nav element with an aria-label of Main Site Links.", () => {
+  expect(getAria.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one main element", () => {
+  expect(getMain.length).toBe(1);
+});
+test("There is at least one footer element", () => {
+  expect(getFooter.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one footer element with the class c-site-footer", () => {
+  expect(getSiteFooter.length).toBe(1);
+});
+test("There is embedded video", () => {
+  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one image", () => {
+  expect(getImage.length).toBeGreaterThanOrEqual(1);
+});
+test("There are at least 500 words on the page", () => {
+  expect(getWords.length).toBeGreaterThanOrEqual(500);
+});
+
+const console = document.getElementById("tests");
+prettify.toHTML(run(), console);
+
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.setAttribute("href", 'html http://www.codeyourfuture.io');
+new_a.innerText = 'CYF';
+
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.setAttribute("href", 'html http://www.codeyourfuture.io');
+new_a.innerText = 'CYF';
+new_li2.appendChild(new_a2);
+
+element_list.appendChild(new_li2);
+let new_li3 = document.createElement('li');
+let new_a3 = document.createElement('a');
+new_a3.setAttribute("href", 'https://www.youtube.com/');
+new_a3.innerText = 'you tube';
+
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.setAttribute("href", 'html http://www.codeyourfuture.io');
+new_a.innerText = 'CYF';
+
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.setAttribute("href", 'html http://www.codeyourfuture.io');
+new_a.innerText = 'CYF';
+new_li2.appendChild(new_a2);
+
+element_list.appendChild(new_li2);
+let new_li3 = document.createElement('li');
+let new_a3 = document.createElement('a');
+new_a3.setAttribute("href", 'https://www.youtube.com/');
+new_a3.innerText = 'you tube';
